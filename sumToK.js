cb776ca2717301369efff36bc46e661ac57f4d45
Joey Steinberger <josamuel@umich.edu>
2018-04-10 15:12:54 -0500

    adding small change

--- sumToK.js
+++ sumToK.js
@@ -1,6 +1,7 @@
 // Given a list of numbers, return whether any two sums to k.
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
+
 function sumsToK(numArr, k) {
     let sumsToK = false;
     numArr.forEach((num, i) => {
